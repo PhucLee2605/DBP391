4ad1a78b9f62a7b75d279286754a71506650e3c6
Kiet Ngo <kietna4@fosft.com.vn>
2022-10-15 18:15:55 +0700

    complete LOF + visual K-distance + add 2 file

--- src/main.py
+++ src/main.py
@@ -1,29 +1,9 @@
-from utils import *
+from ProximityBased import *
 from plot import *
+from utils import *
 import numpy as np
 import pandas as pd
 
 
-normal_data = pd.read_csv('D:/ml/DBP391/archive/ptbdb_abnormal.csv', header=None)
-abnormal_data = pd.read_csv('D:/ml/DBP391/archive/ptbdb_normal.csv', header=None)
-
-X = 10
-Y = 400
-
-abnormal = abnormal_data.iloc[:X,:]
-normal = normal_data.iloc[:Y,:]
-
-data = pd.concat([normal, abnormal], ignore_index=True)
-
-P = int(Y/X) * 2
-res = LOF_algorithm(data, 3, p=P)
-
-#Evaluation LOF model
-y_true = np.concatenate((np.full((1, Y), True), np.full((1,X), False)), axis=1)
-y_pre = np.copy(y_true)
-
-for i in res:
-    y_pre[0][i[0]] = not y_true[0][i[0]]
-
-confusionMatrix(y_true[0], y_pre[0])
+LOF('D:\ml\DBP391/archive\ptbdb_normal.csv', 'D:\ml\DBP391/archive\ptbdb_abnormal.csv')
 
--- src/plot.py
+++ src/plot.py
@@ -2,7 +2,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import seaborn as sns
+from scipy.spatial.distance import cdist
 
+
+
+'''
+    ####################################################################
+    PLOT FOR VISUALIZATION 
+    ####################################################################
+'''
 
 def singlePlot(df, row):
     Y = df.columns.to_list()
@@ -35,10 +43,40 @@ def multiPlot(df, num, Xlabel, Ylabel, fig):
     plt.show()
 
 
+def plotKDistance(X, Y, idx, k):
+    data = [[i, j] for i, j in zip(X, Y)]
+
+    # Find K-distance
+    dist_matrix = cdist(data, data)
+    c = np.copy(dist_matrix[idx])
+    c.sort()
+    kdist = c[k]
+
+    ###PLOT
+    fig, ax = plt.subplots(figsize=(10, 10))
+    ax.scatter(X, Y, s=50)
+
+    # points inside circle
+    points = np.take(data, [np.where(dist_matrix[idx] == i) for i in c[:k + 1]], axis=0)
+    points = np.squeeze(points)
+    ax.scatter(points[:, 0], points[:, 1], s=50, color='r')
+    # Circle
+    cir = plt.Circle(data[idx], kdist, color='black', fill=False)
+    ax.set_aspect('equal', adjustable='datalim')
+    ax.add_patch(cir)
+    ax.set_title(f'K-distance={kdist:.2f} with k={k}', fontsize=22)
+    plt.show()
+
+
+'''
+    ####################################################################
+    PLOT FOR EVALUATION 
+    ####################################################################
+'''
 def confusionMatrix(y_true, y_pre):
     from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
     cm = confusion_matrix(y_true, y_pre)
-
+    print(cm)
     recall = cm[1,1] / (cm[1,1] + cm[1,0]) * 100
     precision = cm[1,1] / (cm[1,1] + cm[0,1]) * 100
 
