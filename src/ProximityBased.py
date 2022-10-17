0c244a20a67965214519fd14b8a87088848d1e0e
Kiet Ngo <kietna4@fosft.com.vn>
2022-10-17 14:48:10 +0700

    change location of GridBased from utils to ProximityBased

--- src/ProximityBased.py
+++ src/ProximityBased.py
@@ -1,8 +1,15 @@
 import pandas as pd
 import numpy as np
+from math import sqrt, ceil, floor
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import MinMaxScaler
 from scipy.spatial.distance import pdist, squareform
 
 
+
+'''
+    Density-based Method
+'''
 # Reachdist function
 def reachdist(distance_df, observation, index):
     return distance_df[observation][index]
@@ -45,3 +52,97 @@ def LOF_algorithm(data_input, k, distance_metric="cityblock", p=5):
         LOF_dict[observation] = LOF
 
     return sorted(LOF_dict.items(), key=lambda x: x[1], reverse=True)[:p]
+
+
+
+'''
+    
+    
+    
+    Grid-based Method
+    
+    
+    
+    
+'''
+
+
+def pca_reduce(data):
+    pca = PCA(n_components=2)
+    reduced_data = pca.fit_transform(data)
+    return reduced_data
+
+
+def normalize(data):
+    min_max_scaler = MinMaxScaler()
+    data_normalized = min_max_scaler.fit_transform(data)
+    return data_normalized
+
+
+class GridBased:
+    def __init__(self, data, r, pi):
+        self.data = pca_reduce(data)
+        self.data = normalize(self.data)
+        self.r = r
+        self.pi = pi
+        self.edge_length = self._get_edge_length()
+        self.grid = [[[] for _ in range(ceil(1 / self.edge_length))]
+                     for _ in range(ceil(1 / self.edge_length))]
+        self.outliers = []
+
+    def assign_points(self):
+        for data_point in self.data:
+            row_idx = floor(data_point[0] / self.edge_length)
+            col_idx = floor(data_point[1] / self.edge_length)
+            self.grid[row_idx][col_idx].append(data_point)
+
+    def assign_single_point(self, point):
+        point = np.array(point)
+        assert np.array(point).shape[-1] == 2, "Yet, only implement grid-based for 2D"
+        row_idx = floor(point[0] / self.edge_length)
+        col_idx = floor(point[1] / self.edge_length)
+        self.grid[row_idx][col_idx].append(point)
+
+    def cells_prune(self):
+        count_total_lv1 = 0
+        for i in range(len(self.grid)):
+            for j in range(len(self.grid)):
+                current_idx = [i, j]
+                neighbors = self.find_neighbor(current_idx)
+                for neighbor in neighbors:
+                    count_total_lv1 += self.count_lv1_objects(current_idx, neighbor)
+                count_total_lv1 += len(self.grid[i][j])  # ! add number of objects in current cell
+
+                if count_total_lv1 <= self.pi * self.data.shape[0]:
+                    self.outliers.extend(self.grid[i][j])
+
+    def _get_edge_length(self):
+        return self.r / 2 * sqrt(self.data.shape[-1])
+
+    def count_lv1_objects(self, current_cell_idx, cell_to_compare):
+        count_no_data_lv1 = 0
+        # print("current",current_cell_idx[0],current_cell_idx[1])
+        # print("compare",cell_to_compare[0],cell_to_compare[1], end = "\n\n")
+        for data_ in self.grid[cell_to_compare[0]][cell_to_compare[1]]:
+            for data in self.grid[current_cell_idx[0]][current_cell_idx[1]]:
+                if np.linalg.norm(data - data_) > self.r:
+                    return 0
+            count_no_data_lv1 += 1
+
+        return count_no_data_lv1
+
+    def find_neighbor(self, cell_idx):
+        neighbors = []
+        row_temp = -2
+        col_temp = -2
+        for _ in range(5):
+            for _ in range(5):
+                if not (row_temp == 0 and col_temp == 0):
+                    idx = idx = [min(max(0, cell_idx[0] + row_temp), len(self.grid) - 1),
+                                 min(max(0, cell_idx[1] + col_temp), len(self.grid) - 1)]
+                    if idx not in neighbors:
+                        neighbors.append(idx)
+                row_temp += 1
+            col_temp += 1
+            row_temp = -2
+        return neighbors
